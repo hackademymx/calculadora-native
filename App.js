3d40afda29a391840e000f396bf22937f792bfde
Jesús García <jesusfgarciavaldez@gmail.com>
2022-02-22 19:26:27 -0700

    calculadora a medio hacer

--- App.js
+++ App.js
@@ -1,11 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import Row from "./components/row";
+import Button from "./components/button";
+
+import React from "react";
+
+//importaremos los archivos
+
+import { actions, initialState, reducer } from "./reducer";
 
 export default function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        <Text style={styles.text}>{state.currentValue}</Text>
+        <Row>
+          <Button
+            text="C"
+            theme="secondary"
+            onPress={() => dispatch({ type: actions.CLICK_C })}
+          />
+          <Button text="+/-" theme="secondary" />
+          <Button text="%" theme="secondary" />
+          <Button text="/" theme="accent" />
+        </Row>
+        <Row>
+          <Button
+            text="7"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 7 })}
+          />
+          <Button
+            text="8"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 8 })}
+          />
+          <Button
+            text="9"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 9 })}
+          />
+          <Button text="X" theme="accent" />
+        </Row>
+        <Row>
+          <Button
+            text="4"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 4 })}
+          />
+          <Button
+            text="5"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 5 })}
+          />
+          <Button
+            text="6"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 6 })}
+          />
+          <Button text="-" theme="accent" />
+        </Row>
+        <Row>
+          <Button
+            text="1"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 1 })}
+          />
+          <Button
+            text="2"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 2 })}
+          />
+          <Button
+            text="3"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 3 })}
+          />
+          <Button
+            text="+"
+            theme="accent"
+            onPress={() =>
+              dispatch({ type: actions.CLICK_OPERATOR, value: "+" })
+            }
+          />
+        </Row>
+        <Row>
+          <Button
+            text="0"
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: 0 })}
+            size="double"
+          />
+          <Button
+            text="."
+            onPress={() => dispatch({ type: actions.CLICK_NUMBER, value: "." })}
+          />
+          <Button
+            text="="
+            theme="secondary"
+            onPress={() => dispatch({ type: actions.CLICK_EQUAL })}
+          />
+        </Row>
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +104,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#202020",
+    paddingTop: 20,
+    justifyContent: "flex-end",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 40,
+    textAlign: "right",
+    marginRight: 20,
+    marginBottom: 10,
   },
 });
---
+++ components/button.jsx
@@ -0,0 +1,64 @@
+import { TouchableOpacity, Text, StyleSheet, Dimensions } from "react-native";
+
+import React from "react";
+
+const screen = Dimensions.get("window");
+
+const buttonWidth = screen.width / 4;
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    backgroundColor: "#333333",
+    height: Math.floor(buttonWidth - 10),
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 5,
+    borderRadius: 45,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 25,
+  },
+  textSecondary: {
+    color: "#060606",
+  },
+  buttonDouble: {
+    width: screen.width,
+    flex: 2,
+    alignItems: "flex-start",
+    paddingLeft: 40,
+  },
+  buttonSecondary: {
+    backgroundColor: "#a6a6a6",
+  },
+  buttonAccent: {
+    backgroundColor: "#f09a36",
+  },
+});
+
+const Button = ({ text, theme, size, onPress }) => {
+  const stylesButton = [styles.button];
+  const stylesText = [styles.text];
+
+  if (size === "double") {
+    stylesButton.push(styles.buttonDouble);
+  }
+
+  if (theme === "secondary") {
+    stylesButton.push(styles.buttonSecondary);
+    stylesText.push(styles.textSecondary);
+  }
+
+  if (theme === "accent") {
+    stylesButton.push(styles.buttonAccent);
+  }
+
+  return (
+    <TouchableOpacity onPress={onPress} style={stylesButton}>
+      <Text style={stylesText}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
